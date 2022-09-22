b437f28a414d2d61b69f0cbba2b70e0909d9e79b
Vito Gamberini <vito@gamberini.email>
2022-09-22 01:15:27 -0400

    Initial commit

---
+++ ReadMe.md
@@ -0,0 +1,9 @@
+# Simply Portraits: No-nonsense Chat Portraits for Foundry VTT
+
+There are a couple mods that add chat portraits to chat messages in FVTT.
+Typically they come with lots of options. This mod has no options.
+Typically chat portrait mods have hundreds or even thousands of lines of code,
+and can be hard to fix when they break. This mod has ~50 lines of code and is
+trivial to keep up-to-date.
+
+That's it. That's the mod.
---
+++ src/main.ts
@@ -0,0 +1,38 @@
+function createImgElement(imgSrc: string): HTMLImageElement {
+  const img: HTMLImageElement = document.createElement("img");
+  img.src = imgSrc;
+  img.height = 36;
+  img.width = 36;
+  img.style.flex = "0 0 0";
+  img.style.border = "none";
+  img.style.marginRight = "5px";
+  img.classList.add('simply-portraits-img');
+  return img;
+}
+
+function getTokenSrc(message: ChatMessage): string | null {
+  const speaker = message.speaker;
+  if (!speaker.token)
+    return null;
+  const scene = game.scenes.get(speaker.scene);
+  const token = scene.tokens.get(speaker.token);
+  return token.texture.src;
+}
+
+Hooks.on(
+  "renderChatMessage", (message, html, data) => {
+    const header = html.find('.message-header')?.[0]
+    const imgSrc = getTokenSrc(message);
+
+    if (!header || !imgSrc)
+      return;
+
+    const img = createImgElement(imgSrc);
+    header.prepend(img);
+
+    const sender = html.find('.message-sender')?.[0]
+    if (!sender)
+      return;
+    sender.style.alignSelf = "center";
+  }
+)
