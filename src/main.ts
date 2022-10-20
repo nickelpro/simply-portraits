84dfbf5aad442c7af9372ec9ba726a2c23ddb537
Vito Gamberini <vito@gamberini.email>
2022-10-20 13:18:54 -0400

    Fix #1, fix #2
    * Switch to using CSS style for most styling stuff
    * Add support for webm extension
    * Use actor to derive token prototype instead of taking token directly
    from scene. Might want to revist this and try both, scene first and then
    actor as a backup.

--- src/main.ts
+++ src/main.ts
@@ -1,39 +1,47 @@
 function createImgElement(imgSrc: string): HTMLImageElement {
   const img: HTMLImageElement = document.createElement("img");
   img.src = imgSrc;
-  img.height = 36;
-  img.width = 36;
-  img.style.flex = "0 0 0";
-  img.style.border = "none";
-  img.style.marginRight = "5px";
-  img.classList.add('simply-portraits-img');
+  img.classList.add('simply-portraits');
   return img;
 }
 
 function getTokenSrc(message: ChatMessage): string | null {
   const speaker = message.speaker;
-  if (!speaker.token)
-    return null;
-  const scene = game.scenes.get(speaker.scene);
-  const token = scene.tokens.get(speaker.token);
-  return token.texture.src;
+  const actor = game.actors.get(speaker.actor);
+  return actor?.prototypeToken?.texture?.src;
+}
+
+function isVideo(imgSrc: string): boolean {
+  const re = /(?:\.([^.]+))?$/;
+  const ext = re.exec(imgSrc)?.[1];
+  return ext === "webm";
+}
+
+function createVideoElement(videoSrc: string): HTMLVideoElement {
+  const video = document.createElement('video');
+  video.src = videoSrc;
+  video.autoplay = false;
+  video.controls = false;
+  video.muted = true;
+  video.classList.add('simply-portraits');
+  return video;
 }
 
 Hooks.on(
   "renderChatMessage", (message, html, data) => {
-    const header = html.find('.message-header')?.[0]
-    const imgSrc = getTokenSrc(message);
+    const header = html.find('.message-header')?.[0];
+    const src = getTokenSrc(message);
 
-    if (!header || !imgSrc)
+    if (!header || !src)
       return;
 
-    const img = createImgElement(imgSrc);
-    header.prepend(img);
-    header.style.paddingBottom = "3px";
+    const portrait = isVideo(src) ? createVideoElement(src) : createImgElement(src);
+    header.prepend(portrait);
+    header.style.paddingBottom = '3px'
 
-    const sender = html.find('.message-sender')?.[0]
+    const sender = html.find('.message-sender')?.[0];
     if (!sender)
       return;
-    sender.style.alignSelf = "center";
+    sender.style.alignSelf = 'center';
   }
 )
